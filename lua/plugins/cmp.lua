beadcc4976743c59c5da6b49acce37060a4f64af
Victor Riveros <victorriba18@hotmail.com>
2024-08-17 08:45:54 -0500

    add supermaven

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,9 +1,17 @@
 return {
   {
     "nvim-cmp",
+    dependencies = {
+      "supermaven-inc/supermaven-nvim",
+      opts = {},
+    },
     opts = function(_, opts)
       local cmp = require("cmp")
-
+      table.insert(opts.sources, 1, {
+        name = "supermaven",
+        group_index = 1,
+        priority = 100,
+      })
       opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
         ["<CR>"] = cmp.config.disable,
         ["<tab>"] = LazyVim.cmp.confirm({ select = true }),
